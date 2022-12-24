97fc627d5a84120a8e315cdb6b59c4486d4fd1f8
eons <eons@eons.dev>
2022-12-23 18:03:54 -0800

    add support for js and css and update workflows

--- inc/wordpress_plugin.py
+++ inc/wordpress_plugin.py
@@ -5,46 +5,39 @@ import re
 from ebbs import Builder
 
 class wordpress_plugin(Builder):
-    def __init__(this, name="Wordpress Plugin Builder"):
-        super().__init__(name)
+	def __init__(this, name="Wordpress Plugin Builder"):
+		super().__init__(name)
 
-        this.clearBuildPath = True
+		this.clearBuildPath = True
 
-        this.supportedProjectTypes.append("plugin")
+		this.supportedProjectTypes.append("plugin")
 
-        this.optionalKWArgs["plugin_name"] = None
-        this.optionalKWArgs["description"] = None
-        this.optionalKWArgs["version"] = "0.0.0"
-        this.optionalKWArgs["author"] = "Web Infrastructure"
-        this.optionalKWArgs["author_uri"] = "https://web.infrastructure.tech"
-        this.optionalKWArgs["license"] = "MIT License"
+		this.optionalKWArgs["plugin_name"] = None
+		this.optionalKWArgs["description"] = None
+		this.optionalKWArgs["version"] = "0.0.0"
+		this.optionalKWArgs["author"] = "Web Infrastructure"
+		this.optionalKWArgs["author_uri"] = "https://web.infrastructure.tech"
+		this.optionalKWArgs["license"] = "MIT License"
 
-    #Required Builder method. See that class for details.
-    def Build(this):
+	#Required Builder method. See that class for details.
+	def Build(this):
 
-        if (this.plugin_name is None):
-            this.plugin_name = this.projectName
+		if (this.plugin_name is None):
+			this.plugin_name = this.projectName
 
-        if (this.description is None):
-            this.description = f"Code for {this.projectName}"
+		if (this.description is None):
+			this.description = f"Code for {this.projectName}"
 
-        this.DetermineSuffix()
+		this.DetermineSuffix()
+		this.WriteMainFile()
 
-        this.targetIncPath = os.path.join(this.buildPath, "inc")
-        try:
-            logging.debug(f"copying {this.incPath} to {this.targetIncPath}")
-            shutil.copytree(this.incPath, this.targetIncPath)
-        except Exception as e:
-            logging.debug(f"{e}")
-        this.WriteMainFile()
+	def DetermineSuffix(this):
+		this.suffix = re.sub('[^A-Za-z0-9]+', '', this.projectName)
 
-    def DetermineSuffix(this):
-        this.suffix = re.sub('[^A-Za-z0-9]+', '', this.projectName)
-
-    def WriteMainFile(this):
-        main_file = this.CreateFile(f"{this.projectName}.php")
-        #NOTE: rglob from: https://stackoverflow.com/questions/17160696/php-glob-scan-in-subfolders-for-a-file
-        main_file.write(
+	def WriteMainFile(this):
+		main_file = this.CreateFile(f"{this.projectName}_main.php")
+		#NOTE: rglob from: https://stackoverflow.com/questions/17160696/php-glob-scan-in-subfolders-for-a-file
+		main_file.write(
 f'''<?php
 /*
 Plugin Name: {this.plugin_name}
@@ -57,20 +50,29 @@ License: {this.license}
 
 function {this.suffix}_rglob($pattern, $flags = 0)
 {{
-    $files = glob($pattern, $flags); 
-    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir)
-    {{
-        $files = array_merge($files, {this.suffix}_rglob($dir.'/'.basename($pattern), $flags));
-    }}
-    return $files;
+	$files = glob($pattern, $flags); 
+	foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir)
+	{{
+		$files = array_merge($files, {this.suffix}_rglob($dir.'/'.basename($pattern), $flags));
+	}}
+	return $files;
 }}
 function {this.suffix}_require_all()
 {{
-    foreach ({this.suffix}_rglob(dirname(__FILE__) . "/inc/*.php") as $filename)
-    {{
-        require_once($filename);
-    }}
+	foreach ({this.suffix}_rglob(dirname(__FILE__) . "/src/*.php") as $filename)
+	{{
+		require_once($filename);
+	}}
+	foreach ({this.suffix}_rglob(dirname(__FILE__) . "/inc/*.css") as $filename)
+	{{
+		wp_enqueue_style("{this.suffix}".$filename, plugins_url($filename, __FILE__));
+	}}
+	foreach ({this.suffix}_rglob(dirname(__FILE__) . "/inc/*.js") as $filename)
+	{{
+		wp_enqueue_script("{this.suffix}".$filename, plugins_url($filename, __FILE__));
+	}}
 }}
 add_action('init', '{this.suffix}_require_all');
+
 ''')
-        main_file.close()
+		main_file.close()
